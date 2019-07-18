d83ebe764218cead4adee7c1f66cf8094e2ed776
Alejandro R. Hernandez <arhernandez@lmds-arher.jomax.paholdings.com>
2019-07-17 21:19:11 -0700

    service worker changes part 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Router from "./components/Router";
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.unregister();
