67824a7a0dd501cc7522ed40edcc26e9c3f0a85e
Alejandro R. Hernandez <arhernandez@lmds-arher.jomax.paholdings.com>
2019-07-17 21:07:56 -0700

    edit 2

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -7,7 +7,7 @@ const Recipes = props => (
     <div className="row">
     { props.recipes.map((recipe) => {
       return (
-        <div key={recipe.title} className="col-md-4" style={{ marginBottom: "2rem" }}>
+        <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }}>
           <div className="recipes__box">
             <img
               className="recipe__box-img"
@@ -15,20 +15,22 @@ const Recipes = props => (
               alt={recipe.title}/>
               <div className="recipe__text">
                 <h5 className="recipes__title">
-                { recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...` }
+                  { recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...` }
                 </h5>
-                <p className="recipes__subtitle">Publisher: <span>{ recipe.publisher }</span></p>
+                <p className="recipes__subtitle">Publisher: <span>
+                  { recipe.publisher }
+                </span></p>
               </div>
               <button className="recipe_buttons">
                 <Link to={{
                   pathname: `/recipe/${recipe.recipe_id}`,
-                  state: { recipe: recipe.title } 
+                  state: { recipe: recipe.title }
                 }}>View Recipe</Link>
               </button>
           </div>
         </div>
       );
-    }) }
+    })}
     </div>
   </div>
 );
